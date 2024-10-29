40ce92d962d09959649da4e8b4ff6954ee283dbb
Dileep <ddr.dileepraloti@gmail.com>
2024-10-29 22:56:09 +0530

    fix : page error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import HeaderNavBar from "@/components/navbar/HeaderNavBar";
+// import HeaderNavBar from "@/components/navbar/HeaderNavBar";
 import Head from "next/head";
 
 const geistSans = localFont({
